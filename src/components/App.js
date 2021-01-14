9697e4913b8557366bd8bacf45ecd52d422c9484
wojciechszutryk <wojtekszutryk@gmail.com>
2021-01-14 23:06:09 +0100

    Finish Result, add error handle

--- src/components/App.js
+++ src/components/App.js
@@ -6,13 +6,20 @@ import './App.css'
 class App extends Component {
   state = {
     city: '',
-    info: '',
-    error: ''
+    weather: '',
+    error: '',
+    temp: '',
+    pressure: '',
+    windSpeed: '',
+    time: '',
+    cityName: ''
   }
 
   handleInputCity = (event) => {
     this.setState({
-      city: event.target.value
+        city: event.target.value,
+        cityName: '',
+        error: false
     })
   }
 
@@ -26,14 +33,21 @@ class App extends Component {
         })
         .then(response => response.json())
         .then(info => {
+            const currentTime = new Date(info.dt * 1000).toLocaleString()
           this.setState({
-            info,
+            city: info.name,
+            weather: info.weather[0].main,
+            temp: info.main.temp,
+            pressure: info.main.pressure,
+            windSpeed: info.wind.speed,
+            time: currentTime,
+            cityName: this.state.city,
             error: false
           })
         })
         .catch(error => {
           this.setState({
-            error: true
+            error: true,
           })
         })
   }
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,17 +1,28 @@
 import React from 'react';
 
 const Result = (props) => {
-    const {error, city} = props.information;
-    const info = (props.information.info.main)
-    console.log(info)
-    console.log(info.temp)
-
+    const { city, weather, error, temp, pressure, windSpeed, time, cityName} = props.information;
+    let information = ''
+    if(!error && !cityName && city) {
+        information = (
+            <div>Press Button to search information about: <strong>{city}</strong></div>
+        )
+    }
+    else if (!error && city){
+        information = (
+            <>
+                <h2>City name: <em>{city}</em></h2>
+                <p>Temperature: <strong>{temp}</strong> &#176;C</p>
+                <p>Pressure: <strong>{pressure}</strong> hPa</p>
+                <p>Wind speed: <strong>{windSpeed}</strong> m/s</p>
+                <p>Current Time: <strong>{time}</strong></p>
+            </>
+        )
+    }
     return (
-        <div>
-            {String(error)}
-            {/*{temp}*/}
-            {/*{pressure}*/}
-        </div>
+        <section  className="weather">
+            {error ? <div>No information about <strong>{city}</strong></div> : information}
+        </section>
     );
 };
 
